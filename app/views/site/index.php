90a746a367c4e322ce02e5ca3e09ccfc46a62c1a
Kristian Skender <kristian@hexis.hr>
2017-09-25 20:25:53 +0200

    Frontend homepage[scemby/front] (#2)
    * Frotnend changes
    
    * Homepage adjustments

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -15,31 +15,42 @@ $this->title = $page->seo('title', $page->model->title);
 
 <?= Carousel::widget(1140, 520) ?>
 
-<div class="text-center">
-    <h1><?= Text::get('index-welcome-title') ?></h1>
-    <p><?= $page->text ?></p>
-</div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <h1><?= Text::get('index-welcome-title') ?></h1>
+      <p><?= $page->text ?></p>
+    </div>
 
-<br/>
-<hr/>
+    <section class="col-sm-12">
+      <h2>Novosti</h2>
 
-<div class="text-center">
-    <h2>Novosti</h2>
-    <div class="row">
-      <div class="col-sm-12">
+      <div class="o-novelty_wrap">
         <div class="row">
           <?php foreach (News::last(3) as $news) { ?>
             <div class="col-sm-4">
-              <?php echo Html::a($news->title, ['news/view', 'slug' => $news->slug]) ?>
-              <br />
-              <img src="<?php echo $news->image; ?>" height="150px" width="200px" />
-              <br />
-              <?php echo $news->short ?>
+              <!-- TODO: add link to novelty -->
+              <a class="o-novelty_link" href="#">
+                <article class="o-novelty">
+                  <h2 class="o-novelty_title"><strong><?php echo $news->title; ?></strong></h2>
+
+                  <!-- TODO: add IF condition so we don't have img element without source if image isn't uploaded -->
+                  <img class="o-novelty_img" src="<?php echo $news->image; ?>">
+
+                  <div class="o-novelty_text">
+                    <?php echo $news->short ?>
+                  </div>
+                </article>
+              </a>
             </div>
           <?php } ?>
         </div>
       </div>
-    </div>
+
+    </section>
+  </div>
 </div>
 
-<br/>
+<br>
+<hr>
+<br>
