d35f8009da571a3c66fd36d6851d88cd6f1e51ff
Kristian Skender <scemby@gmail.com>
2017-09-26 18:45:38 +0200

    Homepage adjustments

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -14,42 +14,45 @@ $this->title = $page->seo('title', $page->model->title);
 
 <?= Carousel::widget(1140, 520) ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm-12 text-center">
-      <h1><?= Text::get('index-welcome-title') ?></h1>
-      <p><?= $page->text ?></p>
-    </div>
-
-    <section class="col-sm-12">
-      <h2>Novosti</h2>
-
-      <div class="o-novelty_wrap">
-        <div class="row">
-          <?php foreach (News::last(3) as $news) { ?>
-            <div class="col-sm-4">
-              <a class="o-novelty_link" href="<?php echo Url::to(['/news/view', 'slug' => $news->slug]); ?>">
-                <article class="o-novelty">
-                  <h2 class="o-novelty_title"><strong><?php echo $news->title; ?></strong></h2>
-
-                  <?php if ($news->image) { ?>
-                    <img class="o-novelty_img" src="<?php echo $news->image; ?>">
-                  <?php } ?>
-
-                  <div class="o-novelty_text">
-                    <?php echo $news->short ?>
-                  </div>
-                </article>
-              </a>
-            </div>
-          <?php } ?>
-        </div>
+<div class="o-welcome">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <h1 class="o-welcome_title"><?= Text::get('index-welcome-title') ?></h1>
+        <div class="o-welcome_text"><?= $page->text ?></div>
       </div>
-
-    </section>
+    </div>
   </div>
 </div>
 
-<br>
-<hr>
-<br>
+<div class="o-novelty_sectionWrap">
+  <section class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h2>Novosti</h2>
+
+        <div class="o-novelty_wrap">
+          <div class="row">
+            <?php foreach (News::last(3) as $news) { ?>
+              <div class="col-sm-4">
+                <a class="o-novelty_link" href="<?php echo Url::to(['/news/view', 'slug' => $news->slug]); ?>">
+                  <article class="o-novelty">
+                    <h2 class="o-novelty_title"><strong><?php echo $news->title; ?></strong></h2>
+
+                    <?php if ($news->image) { ?>
+                      <img class="o-novelty_img" src="<?php echo $news->image; ?>">
+                    <?php } ?>
+
+                    <div class="o-novelty_text">
+                      <?php echo $news->short ?>
+                    </div>
+                  </article>
+                </a>
+              </div>
+            <?php } ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
