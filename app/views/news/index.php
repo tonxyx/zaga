c4e993a122125ff72713176f9d2bb55357804aa9
Kristian Skender <scemby@gmail.com>
2017-09-27 18:03:47 +0200

    News list page frontend adjustments

--- app/views/news/index.php
+++ app/views/news/index.php
@@ -9,26 +9,50 @@ $page = Page::get('page-news');
 $this->title = $page->seo('title', $page->model->title);
 $this->params['breadcrumbs'][] = $page->model->title;
 ?>
-<h1><?= $page->seo('h1', $page->title) ?></h1>
-<br/>
 
-<?php foreach($news as $item) : ?>
-    <div class="row">
-        <div class="col-md-2">
-            <?= Html::img($item->thumb(160, 120)) ?>
-        </div>
-        <div class="col-md-10">
-            <?= Html::a($item->title, ['news/view', 'slug' => $item->slug]) ?>
-            <div class="small-muted"><?= $item->date ?></div>
-            <p><?= $item->short ?></p>
-            <p>
-                <?php foreach($item->tags as $tag) : ?>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+
+      <h1><?= $this->title ?></h1>
+
+      <ul class="o-news">
+
+
+        <?php foreach($news as $item) : ?>
+          <li class="o-news_item">
+            <div class="row">
+              <div class="col-md-2">
+                <!-- TODO add img with according class -->
+                <!-- <img class="o-news_img" src="" alt="">-->
+                <?= Html::img($item->thumb(160, 120)) ?>
+              </div>
+
+              <div class="col-md-10">
+
+                <!-- TODO add item title and link to it -->
+                <a class="o-news_link" href="#">Some static title which needs to be changed</a>
+
+                <div class="o-news_date"><?= $item->date ?></div>
+
+                <div class="o-news_desc"><?= $item->short ?></div>
+
+                <div>
+                  <?php foreach($item->tags as $tag) : ?>
                     <a href="<?= Url::to(['/news', 'tag' => $tag]) ?>" class="label label-info"><?= $tag ?></a>
-                <?php endforeach; ?>
-            </p>
-        </div>
+                  <?php endforeach; ?>
+                </div>
+
+              </div>
+            </div>
+          </li>
+        <?php endforeach; ?>
+
+      </ul>
+
+
     </div>
-    <br>
-<?php endforeach; ?>
+  </div>
+</div>
 
 <?= News::pages() ?>
