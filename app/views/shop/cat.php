2ccafc4beba722e9d0c65ca603011b8872262028
root <root@debian-racki.localdomain>
2019-09-11 19:12:05 +0000

    Category side menu

--- app/views/shop/cat.php
+++ app/views/shop/cat.php
@@ -2,13 +2,34 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\widgets\ActiveForm;
+use yii\easyii\modules\catalog\api\Catalog;
 
 $this->title = $cat->seo('title', $cat->model->title);
 $this->params['breadcrumbs'][] = ['label' => 'Katalog', 'url' => ['shop/index']];
 $this->params['breadcrumbs'][] = $cat->model->title;
 ?>
 
-<div class="container">
+<style>
+.list-group {
+list-style: none;
+}
+.o-catalog {
+  width: 60%;
+  float: right;
+  color: #71777a;
+}
+.o-categoryList a {
+  color: #71777a;
+}
+.list-group-collapse {
+  padding-left: 10px;
+}
+.list-group-collapse a {
+  color: #e5803a;
+}
+</style>
+
+	<div class="container">
   <div class="row">
     <div class="col-sm-12">
       <div class="o-categoryList">
@@ -25,17 +46,51 @@ $this->params['breadcrumbs'][] = $cat->model->title;
             <?php endif; ?>
           </div>
 
-          <div class="col-md-4">
-            <h4>Filter</h4>
-            <div class="well well-sm">
+	  <div class="col-md-4">
+<div class="row">
+	    <div class="col-12" style="text-align: right">
+            <a class="btn btn-default" data-toggle="collapse" href="#collapseE" role="button" aria-expanded="false" aria-controls="collapseExample">Filter</a>
+	    </div>
+	    <div class="col-12 well well-sm collapse" id="collapseE">
               <?php $form = ActiveForm::begin(['method' => 'get', 'action' => Url::to(['/shop/cat', 'slug' => $cat->slug])]); ?>
                 <?= $form->field($filterForm, 'priceFrom') ?>
                 <?= $form->field($filterForm, 'priceTo') ?>
-                <?= $form->field($filterForm, 'storageFrom') ?>
-                <?= $form->field($filterForm, 'storageTo') ?>
+                <?php //$form->field($filterForm, 'storageFrom') ?>
+                <?php // $form->field($filterForm, 'storageTo') ?>
                 <?= Html::submitButton('Traži', ['class' => 'btn btn-primary']) ?>
               <?php ActiveForm::end(); ?>
-            </div>
+	    </div>
+<hr>
+<?php
+   function renderNode($node, $initialImage = true){
+        if(!count($node->children)){
+					        $html = '<li class="list-item">';
+        $html .= Html::a($node->title, ['/shop/cat', 'slug' => $node->slug]) . '</li>';
+    } else {
+      $html = '<li class="list-item">';
+
+      $html .= '<a href="#' . $node->slug . '" class="" data-toggle="collapse">' . $node->title . '<span class="pull-right">↓</span></a>';
+
+      $html .= '<ul class="list-group list-group-collapse collapse" id="' . $node->slug . '">';
+
+      foreach($node->children as $child){
+        $html .= renderNode($child, false);
+      }
+
+      $html .= '</ul></li>';
+    }
+
+    return $html;
+   } ?>
+
+<br>
+<div class="o-catalog">
+	  <ul class="list-group">
+            <?php foreach(Catalog::tree() as $node) echo renderNode($node); ?>
+          </ul>
+
+        </div>
+</div>
           </div>
         </div>
       </div>
